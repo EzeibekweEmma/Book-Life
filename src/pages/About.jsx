aab89f44f491562ff0377ecdbb36aed5fea330f5
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-05-15 12:50:27 +0100

    added an empty About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+
+   return (
+    <section className='flex flex-col p-7 text-center text-6xl bg-slate-700 text-white'>
+      <h2>About Page</h2>
+    </section>
+  )
+}
+
+
+export default About;
