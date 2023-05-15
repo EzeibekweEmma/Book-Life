d0836d8cfd896a3ce79b90b06402652b90250ffb
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-05-15 12:47:58 +0100

    created the Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-800 text-gray-400 font-thin text-sm text-center py-4">
+      © 2023 #BOOKLIFE
+    </footer>
+  );
+}
+
+export default Footer
